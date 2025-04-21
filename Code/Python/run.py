2ccfce6a1aa443ec4ab2d01ffc5cc6250679295a
Zhengyang Liu <liuzy19911112@gmail.com>
2025-04-21 12:38:24 -0400

    Batch report script
    Look for all the "early" folders and run "report_early" script.

--- Code/Python/run.py
+++ Code/Python/run.py
@@ -1,16 +1,12 @@
 import os
 
-folder = r"G:\My Drive\Research projects\F\Data\06282024\tracking\exp5\26-29"
+folder = r"G:\My Drive\Research projects\F\Data"
 
-params = {}
-params["minThreshold"] = [0]
-params["maxThreshold"] = [250]
-params["circularity"] = [0.8]
-params["convexity"] = [.5]
-params["inertia"] = [.5]
+sfL = next(os.walk(folder))[1]
 
-cmd = f"python find_drops.py \"{folder}\""
-for kw in params:
-    for value in params[kw]:
-        cmd += f" --{kw} {value}"
-os.system(cmd)
+for sf in sfL:
+    ssfL = next(os.walk(os.path.join(folder, sf)))[1]
+    for ssf in ssfL:
+        if "early" in ssf:
+            os.system("python report_early.py \"{}\" -n 8 -o 0.3".format(os.path.join(folder, sf, ssf, "crop")))
+        
