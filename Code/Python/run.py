9d998466ae64ae4268eebd389812bd49cbb3a34f
Zhengyang Liu <liuzy19911112@gmail.com>
2025-04-24 12:41:57 -0400

    Compute flux for multiple trials

--- Code/Python/run.py
+++ Code/Python/run.py
@@ -8,5 +8,11 @@ for sf in sfL:
     ssfL = next(os.walk(os.path.join(folder, sf)))[1]
     for ssf in ssfL:
         if "early" in ssf:
-            os.system("python report_early.py \"{}\" -n 8 -o 0.3".format(os.path.join(folder, sf, ssf, "crop")))
+            early_folder = os.path.join(folder, sf, ssf, "crop")
+            # if os.path.exists(os.path.join(early_folder, "nrvf.h5")):
+            #     print(f"{sf}/{ssf} Report already exists, skipping.")
+            #     continue
+            # else:
+            #     print(f"Processing {sf}/{ssf}")
+            os.system("python report_early.py \"{}\" -n 8 -o 0.3".format(early_folder))
         
