60425590f70e17d426b4190a7e6a0a4e15005c09
Zhengyang Liu <liuzy19911112@gmail.com>
2025-05-27 15:45:16 -0400

    process data before 05272025

--- Code/Python/run.py
+++ Code/Python/run.py
@@ -9,10 +9,10 @@ for sf in sfL:
     for ssf in ssfL:
         if "early" in ssf:
             early_folder = os.path.join(folder, sf, ssf, "crop")
-            # if os.path.exists(os.path.join(early_folder, "nrvf.h5")):
-            #     print(f"{sf}/{ssf} Report already exists, skipping.")
-            #     continue
-            # else:
-            #     print(f"Processing {sf}/{ssf}")
-            os.system("python report_early.py \"{}\" -n 8 -o 0.3".format(early_folder))
+            if os.path.exists(os.path.join(early_folder, "nrvf.h5")):
+                print(f"{sf}/{ssf} Report already exists, skipping.")
+                continue
+            else:
+                print(f"Processing {sf}/{ssf}")
+                os.system("python report_early.py \"{}\" -n 8 -o 0.3".format(early_folder))
         
